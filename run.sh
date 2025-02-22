0cbf3778a1db8bedfbed142b2bfac6d105d091a8
Erik Abair <erik.abair@gmail.com>
2025-02-21 21:17:13 -0800

    Adds ability to run the dashboard.

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -eu
 
-if [[ $# -lt 1 ]]; then
-  echo "Usage: $0 <xiso_path>"
-  exit 1
+command=(
+  ./build/qemu-system-i386
+)
+
+if [[ $# -eq 1 ]]; then
+  command+=(
+    -dvd_path "$1"
+  )
 fi
 
 script_dir="$(dirname "${0}")"
 readonly script_dir
 
 darwin_lib_subpath="${script_dir}/dist/xemu.app/Contents/Libraries/$(uname -m)"
-DYLD_FALLBACK_LIBRARY_PATH=$(realpath "${darwin_lib_subpath}") ./build/qemu-system-i386 -dvd_path "$1"
+DYLD_FALLBACK_LIBRARY_PATH=$(realpath "${darwin_lib_subpath}") "${command[@]}"
+
